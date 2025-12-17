06a40158c25d5b2b88df9f2c15fdd191d240d667
qingbingwei <zjb1225212252@gmail.com>
2025-12-17 17:49:51 +0800

    first commit

---
+++ langchain-study/langchain-demo.ts
@@ -0,0 +1,32 @@
+import { createAgent,tool } from "langchain";
+import { ChatDeepSeek } from "@langchain/deepseek";
+import { appConfig } from "../config/config";
+
+import * as z from "zod";
+
+const getWeather = tool(
+    (input) => `It is sunny in ${input.city}!`,
+    {
+        name: "get_weather",
+        description: "Get the weather for a given city",
+        schema: z.object({
+            city: z.string().describe("The city to get the weather for"),
+        }),
+    }
+)
+
+const deepseekChatModel = new ChatDeepSeek({
+  model: "deepseek-chat",
+  apiKey: appConfig.openaiApiKey,
+})
+
+const agent = createAgent({
+    model: deepseekChatModel,
+    tools: [getWeather],
+})
+
+console.log(
+  await agent.invoke({
+    messages: [{ role: "user", content: "What's the weather in Tokyo?" }],
+  })
+);
